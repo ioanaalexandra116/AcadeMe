b039fc12c87ed48b8395be16d8e924b78977f8aa
Ioana-Alexandra <ioanaalexandra116@gmail.com>
2024-03-16 19:02:55 +0200

    updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
-import * as React from "react";
-import { useState } from "react";
-import { cn } from "@/lib/utils";
-import { useLocation } from "react-router-dom"; // Import useLocation hook
+"use client"
+
+import * as React from "react"
+import Logo from "../assets/title.svg"
+
+import { cn } from "@/lib/utils"
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -9,103 +11,121 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
 
-const search = [
-  {
-    title: "Find Flashcard Sets",
-    href: "/serach/flashcard-sets",
-    description: "Search for flashcard sets created by other users",
-  },
+import Avatar from "./Avatar"
+
+const components: { title: string; href: string; description: string }[] = [
   {
     title: "Find People",
     href: "/search/people",
-    description: "Connect with other study enthusiasts",
+    description:
+      "Connect with other study enthusiasts",
   },
-];
+  {
+    title: "Find Flashcard Sets",
+    href: "/search/flashcards",
+    description:
+      "Discover flashcard sets created by other users",
+  },
+]
 
-const Navbar = () => {
-  const location = useLocation(); // Get the current location
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
+export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 z-50 w-full bg-transparent backdrop-blur-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="hidden md:block">
-          <div className="ml-10 flex items-baseline space-x-4 justify-center">
-            <NavigationMenu className="flex items-center">
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <NavigationMenuLink href="/">Dashboard</NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuLink href="/profile">Profile</NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuLink href="/create">Create</NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger
-                    onClick={toggleMenu}
-                    className="cursor-pointer text-#9B7960 hover:text-#9B7960 focus:outline-none focus:text-#9B7960"
+    <NavigationMenu className="backdrop-blur-lg fixed z-50 bg-transparent shadow-md">
+      <NavigationMenuItem className="flex justify-start items-left absolute left-0">
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <img src={Logo} alt="logo" className="h-8 w-24" />
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      <NavigationMenuList className="mb-2 mt-2 flex justify-center items-center">
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent className="flex justify-center items-center">
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
                   >
-                    Search
-                  </NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                      {search.map((component) => (
-                        <ListItem
-                          key={component.title}
-                          title={component.title}
-                          href={component.href}
-                          isActive={location.pathname === component.href}
-                        >
-                          {component.description}
-                        </ListItem>
-                      ))}
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      shadcn/ui
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Beautifully designed components built with Radix UI and
+                      Tailwind CSS.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Search</NavigationMenuTrigger>
+          <NavigationMenuContent className="flex justify-center items-center">
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] flex items-center justify-center">
+              {components.map((component) => (
+                <ListItem className="flex items-center justify-center"
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuItem className="flex items-right justify-end absolute right-0">
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <img src={Logo} alt="logo" className="h-8 w-24" />
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+    </NavigationMenu>
+  )
+}
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a"> & { isActive?: boolean } // Add isActive prop
->(({ className, title, children, isActive, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors",
-            {
-              "font-bold bg-white": isActive,
-              "hover:bg-accent focus:bg-accent focus:text-#9B7960": !isActive,
-            },
+
+            "flex items-center justify-center block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
           {...props}
         >
           <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-1 text-sm leading-snug text-muted-foreground">
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
         </a>
       </NavigationMenuLink>
     </li>
-  );
-});
-ListItem.displayName = "ListItem";
-export default Navbar;
+  )
+})
+ListItem.displayName = "ListItem"
